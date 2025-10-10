7fef00108ca5f6d8ecdd62196e291eb7934b5b6d
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-10-10 05:31:28 -0400

    Update Site

--- config.js
+++ config.js
@@ -198,7 +198,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1760088675375_0",
+      "id": "q_auto_1760088689230_0",
       "questionText": "When are you planning to make your next move?",
       "options": [
         {
@@ -216,7 +216,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1760088675375_1",
+      "id": "q_auto_1760088689230_1",
       "questionText": "Have you been pre-approved for a mortgage, or would you like a connection to a trusted mortgage advisor?",
       "options": [
         {
@@ -234,7 +234,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1760088675375_2",
+      "id": "q_auto_1760088689230_2",
       "questionText": "Is your current mortgage up for renewal soon?",
       "options": [
         {
@@ -252,7 +252,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1760088675375_3",
+      "id": "q_auto_1760088689230_3",
       "questionText": "Which best describes your property needs right now?",
       "options": [
         {
